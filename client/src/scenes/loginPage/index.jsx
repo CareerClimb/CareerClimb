6e75cf4c987fb9e78bce04f5bcacb9697c2c8926
Navdeep Singh <navdeep5@ualberta.ca>
2024-06-11 19:52:44 -0600

    Altering landingpage to contain register and start

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,10 +1,13 @@
 // Import packages
-import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import { Box, Typography, useTheme, useMediaQuery, Button } from '@mui/material';
 import BottomBar from 'scenes/bottomBar';
 import TopBar from 'scenes/topBar';
+import React from 'react';
+import Form from './Form';
 
 const LoginPage = () => {
   const theme = useTheme();
+  const { palette } = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
@@ -19,24 +22,60 @@ const LoginPage = () => {
       <Box
         sx={{
           display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          flexDirection: isMobile ? 'column' : 'row',
           flex: 1,
-          padding: isMobile ? 2 : 4,
         }}
       >
-        <Typography variant="h2" gutterBottom>
-          Welcome to the Login Page
-        </Typography>
-        <Typography variant="body1">
-          This is a simple login page
-        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flex: 1,
+            padding: isMobile ? 2 : 4,
+            borderRight: isMobile ? 'none' : '1px solid #ccc',
+            borderBottom: isMobile ? '1px solid #ccc' : 'none',
+          }}
+        >
+          <Form />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flex: 1,
+            padding: isMobile ? 2 : 4,
+          }}
+        >
+          <Typography variant="h2" gutterBottom>
+            CareerClimb
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            Welcome! Begin your journey
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ marginTop: 2,
+              m: "2rem 0",
+              // p: "1rem",
+              backgroundColor: palette.primary.main,
+              color: palette.background.alt,
+              borderRadius: "20px",
+              "&:hover": { color: palette.primary.main },
+             }}
+            
+          >
+            Start your Climb Today
+          </Button>
+        </Box>
       </Box>
       <BottomBar />
     </Box>
   );
 }
-
 
 export default LoginPage;
