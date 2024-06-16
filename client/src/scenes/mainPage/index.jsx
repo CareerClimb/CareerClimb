598ef29d6a5ca216bf3465972251be8d0652b777
Navdeep Singh <navdeep5@ualberta.ca>
2024-06-16 15:54:35 -0600

    Updated theme and text for the Main Page

--- client/src/scenes/mainPage/index.jsx
+++ client/src/scenes/mainPage/index.jsx
@@ -2,44 +2,39 @@
 import React from 'react';
 import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
 import TopBar from 'scenes/topBar';
-import BottomBar from 'scenes/bottomBar';
 import SearchBar from 'components/SearchBar';
 import PostsWidget from 'scenes/widgets/PostsWidget';
 
 const MainPage = () => {
     const theme = useTheme();
+    const { palette } = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-    return (
+return (
+    <Box
+    sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '20px',
+        textAlign: 'center',
+    }}>
+        <TopBar />
         <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-                textAlign: 'center',
-            }}
-        >
-            <TopBar />
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    flexGrow: 1, // Take up remaining space
-                }}
-            >
-                <Typography variant="h2" gutterBottom>
-                    CareerClimb.
-                </Typography>
-                <Typography variant="h5" gutterBottom>
-                    Welcome! Search for the jobs you are looking for.
-                </Typography>
-                <SearchBar />
-                <PostsWidget />
-            </Box>
-            <BottomBar />
+        sx={{
+            padding: isMobile ? 8 : 15,
+        }}
+      >
+            <Typography variant="h1" fontSize='62px' fontWeight='bold' marginBottom="3px">
+                CareerClimb.
+            </Typography>
+            <Typography variant="h4" marginBottom='1.5rem' fontFamily="Roboto" color={palette.neutral.medium}>
+                Welcome! Search for the jobs you are looking for.
+            </Typography>
+            <SearchBar />
+            <PostsWidget />
         </Box>
-    );
+    </Box>
+);
 };
 
 export default MainPage;
