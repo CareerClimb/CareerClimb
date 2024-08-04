a295e8fbf935a53921b062ff35cb34317a666e1b
Navdeep Singh <navdeep5@ualberta.ca>
2024-08-03 19:01:34 -0600

    Added years as an option of when jobs were posted

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -51,8 +51,12 @@ const PostsWidget = () => {
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
     const months = Math.floor(days / 30);
+    const years = Math.floor(months / 12);
 
-    if (months > 0) {
+
+    if (years > 0) {
+      return `${years} year${years > 1 ? 's' : ''} ago`;
+    } else if (months > 0) {
       return `${months} month${months > 1 ? 's' : ''} ago`;
     } else if (days > 0) {
       return `${days} day${days > 1 ? 's' : ''} ago`;
