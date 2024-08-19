82f91c5287811ef067385d23367bc437f14604d2
Dylan <dndu@ualberta.ca>
2024-08-18 23:02:33 -0600

    removed unused function

--- server/index.js
+++ server/index.js
@@ -50,27 +50,6 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
 });
 
-
-async function removeOldestDocuments() {
-    try {
-        // Find the 10,000 oldest documents
-        const oldestDocs = await Job.find({})
-            .sort({ createdAt: 1 }) // Sort in ascending order to get the oldest first
-            .limit(10000) // Limit to the 10,000 oldest documents
-            .exec();
-
-        // Extract the _id values of the documents to be deleted
-        const idsToDelete = oldestDocs.map(doc => doc._id);
-
-        // Remove the documents
-        await Job.deleteMany({ _id: { $in: idsToDelete } });
-
-        console.log('Removed 10,000 oldest documents successfully.');
-    } catch (error) {
-        console.error('Error removing documents:', error);
-    }
-}
-
 /* MONGOOSE SETUP */
 mongoose
     .connect(MONGO_URL, {
@@ -79,7 +58,6 @@ mongoose
     })
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-        //removeOldestDocuments();
     })
     .catch((error) => console.log(`${error} did not connect`));
 
